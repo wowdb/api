e9fd6f19b7117bc0f1638a67c13382e93cbf8683
Ali Zahid <ali.zahid@live.com>
2018-03-30 23:58:51 +0400

    New Relic added

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+const { NEW_RELIC_LICENSE_KEY } = process.env
+
+exports.config = {
+  app_name: ['Mr Bigglesworth'],
+  license_key: NEW_RELIC_LICENSE_KEY,
+  logging: {
+    level: 'info'
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*'
+    ]
+  }
+}
